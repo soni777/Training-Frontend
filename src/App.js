8998e5badd9fd57c2d35ce42c3a24de443940271
sonica <sonicaanapu@gmail.com>
2023-09-02 15:39:11 +0530

    welcome commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to the Training Program
+      </header>
+    </div>
+  );
+}
+
+export default App;
